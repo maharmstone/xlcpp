a6f3af63c544554064e3e3fbd2342cde0bfa0def
Mark Harmstone <mark@harmstone.com>
2020-05-14 15:00:30 +0100

    write test XML

--- xlcpp.cpp
+++ xlcpp.cpp
@@ -1,6 +1,7 @@
 #include "xlcpp.h"
 #include <archive.h>
 #include <archive_entry.h>
+#include <libxml/xmlwriter.h>
 
 using namespace std;
 
@@ -11,8 +12,56 @@ sheet& workbook::add_sheet(const string& name) {
 }
 
 string sheet::xml() const {
-    // FIXME
-    return "<test>Lemon curry?</test>";
+    int rc;
+    string ret;
+    xmlBufferPtr buf;
+    xmlTextWriterPtr writer;
+
+    buf = xmlBufferCreate();
+    if (!buf)
+        throw runtime_error("xmlBufferCreate failed");
+
+    try {
+        writer = xmlNewTextWriterMemory(buf, 0);
+        if (!writer)
+            throw runtime_error("xmlNewTextWriterMemory failed");
+
+        try {
+            rc = xmlTextWriterStartDocument(writer, nullptr, "UTF-8", nullptr);
+            if (rc < 0)
+                throw runtime_error("xmlTextWriterStartDocument failed (error " + to_string(rc) + ")");
+
+            rc = xmlTextWriterStartElement(writer, BAD_CAST "test");
+            if (rc < 0)
+                throw runtime_error("xmlTextWriterStartElement failed (error " + to_string(rc) + ")");
+
+            rc = xmlTextWriterWriteString(writer, BAD_CAST "he&<llo");
+            if (rc < 0)
+                throw runtime_error("xmlTextWriterWriteString failed (error " + to_string(rc) + ")");
+
+            rc = xmlTextWriterEndElement(writer);
+            if (rc < 0)
+                throw runtime_error("xmlTextWriterEndElement failed (error " + to_string(rc) + ")");
+
+            rc = xmlTextWriterEndDocument(writer);
+            if (rc < 0)
+                throw runtime_error("xmlTextWriterEndDocument failed (error " + to_string(rc) + ")");
+        } catch (...) {
+            xmlFreeTextWriter(writer);
+            throw;
+        }
+
+        xmlFreeTextWriter(writer);
+    } catch (...) {
+        xmlBufferFree(buf);
+        throw;
+    }
+
+    ret = (char*)buf->content;
+
+    xmlBufferFree(buf);
+
+    return ret;
 }
 
 void sheet::write(struct archive* a) const {
@@ -20,12 +69,13 @@ void sheet::write(struct archive* a) const {
     string data = xml();
 
     entry = archive_entry_new();
-    archive_entry_set_pathname(entry, ("xl/worksheets/sheet" + to_string(num) + ".xml").c_str());
+    archive_entry_set_pathname(entry, ("xl/worksheets/sheet" + to_string(num) + ".txt").c_str()); // FIXME
     archive_entry_set_size(entry, data.length());
     archive_entry_set_filetype(entry, AE_IFREG);
     archive_entry_set_perm(entry, 0644);
     archive_write_header(a, entry);
     archive_write_data(a, data.data(), data.length());
+    // FIXME - set date?
     archive_entry_free(entry);
 }
 
--- xltest.cpp
+++ xltest.cpp
@@ -1,10 +1,22 @@
 #include <xlcpp.h>
+#include <iostream>
 
-int main() {
+using namespace std;
+
+static void main2() {
     xlcpp::workbook wb;
 
     auto sh = wb.add_sheet("Sheet1");
     wb.save("out.xlsx");
+}
+
+int main() {
+    try {
+        main2();
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
